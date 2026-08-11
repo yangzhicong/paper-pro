8e71624a09dda92a0e3b1a21fe9988102826a578
yangzhicong <78916531+yangzhicong@users.noreply.github.com>
2026-08-11 15:22:29 +0800

    Update default HY2_PORT from 24006 to 24112

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -52,7 +52,7 @@ public class App {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "24006");
+    private static final String HY2_PORT = env("HY2_PORT", "24112");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
