4a1d5d754ccc94097b2b51e243d4cf328aee26b9
yangzhicong <78916531+yangzhicong@users.noreply.github.com>
2026-08-24 21:17:34 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -48,8 +48,8 @@ public class App {
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "mcserverhost.ycong.dpdns.org");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiNmE4NTdkMDVjOTk4MWQyZjQyYTg2NTZkM2YyOGE3YTMiLCJ0IjoiOTI0ODc4M2ItNjMyZS00ODliLTlmNzItMzdiNjQ0MzA3MmJkIiwicyI6Ik1URXhNbVUzWmpNdE5UaGtaUzAwTnpVMkxXRmlaV0l0TnpWbVpXSTRNRGxoT0RVeSJ9");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "14609");
